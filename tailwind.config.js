24b6f7839cb0fe6163f9daf06fd345bad5666931
Matt Rose <mattroseinbox@gmail.com>
2021-05-15 04:41:07 +0430

    feat: add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"]
+    }
   },
   variants: {
     extend: {}
