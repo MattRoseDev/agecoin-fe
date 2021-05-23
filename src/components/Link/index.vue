391af90ad32f1ab593fbed40288ae1688b7c0876
Mahdi <mahdineer@pm.me>
2021-05-23 15:13:06 +0430

    feat: add Link component

---
+++ src/components/Link/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link class="link" :to="to" :class="className">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Link",
+  props: {
+    to: String,
+    className: String,
+  },
+});
+</script>
+<style src="./style.css" scoped></style>
