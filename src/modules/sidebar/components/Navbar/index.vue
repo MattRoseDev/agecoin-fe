f2048f43d8f0ec3a6492054ce2bd491a236c8ff1
Mahdi <mahdineer@pm.me>
2021-05-24 20:34:44 +0430

    feat: add sidebar module

---
+++ src/modules/sidebar/components/Navbar/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="fixed bottom-0 w-full z-20">
+    <div
+      class="flex flex-row-reverse justify-around items-stretch bg-white shadow border-t"
+    >
+      <router-link
+        class="flex justify-center items-center w-full"
+        to="/profile"
+      >
+        <Avatar className="w-6 h-6" />
+      </router-link>
+      <router-link
+        v-for="item in navigation"
+        class="py-2 w-full flex justify-center"
+        :key="item.name"
+        :to="item.to"
+        v-slot="{ isActive }"
+        exact
+      >
+        <component
+          :is="item.icon"
+          :class="[isActive ? 'active-icon' : 'inactive-icon', 'icon']"
+          aria-hidden="true"
+        />
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { CollectionIcon, ViewGridIcon } from "@heroicons/vue/outline";
+import Avatar from "@/components/Avatar";
+
+export default defineComponent({
+  name: "NavbarComponent",
+  components: {
+    Avatar,
+    CollectionIcon,
+    ViewGridIcon
+  },
+  setup() {
+    const navigation = [
+      {
+        name: "Dashboard",
+        to: "/dashboard",
+        icon: ViewGridIcon
+      },
+      {
+        name: "Tasks",
+        to: "/tasks",
+        icon: CollectionIcon
+      }
+    ];
+    return { navigation };
+  }
+});
+</script>
+<style src="./style.css" scoped></style>
---
+++ src/modules/sidebar/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="hidden lg:flex items-start py-5 h-full">
+      <div class="flex flex-col w-72 shadow bg-white rounded-lg top-0 sticky">
+        <div
+          class="flex items-center px-5 py-4 font-bold border-b boder-gray-900"
+        >
+          Time Wallet
+        </div>
+
+        <div class="h-0 flex-1 flex flex-col overflow-y-auto pt-5 pb-2">
+          <div>
+            <span class="flex w-full justify-between items-center px-4 ">
+              <router-link
+                to="/profile"
+                class="flex min-w-0 items-center justify-between space-x-3"
+              >
+                <Avatar className="w-10 h-10" />
+                <span class="flex-1 flex flex-col min-w-0">
+                  <span class="text-gray-900 text-sm font-medium truncate">
+                    Edward
+                  </span>
+                  <span class="text-gray-500 text-sm truncate">@favecode</span>
+                </span>
+              </router-link>
+            </span>
+          </div>
+          <Navigation />
+        </div>
+      </div>
+    </div>
+    <div class="lg:hidden">
+      <Navbar />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Avatar from "@/components/Avatar/index.vue";
+import Navigation from "./components/Navigation/index.vue";
+import Navbar from "./components/Navbar/index.vue";
+
+export default defineComponent({
+  name: "SidebarFeature",
+  components: {
+    Avatar,
+    Navigation,
+    Navbar
+  }
+});
+</script>
