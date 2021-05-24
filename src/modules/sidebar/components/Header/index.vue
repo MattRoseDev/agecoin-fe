254aa82bb790d8e966365e41acfb373826755f10
Matt Rose <mattroseinbox@gmail.com>
2021-05-24 20:34:44 +0430

    feat: add sidebar module

---
+++ src/modules/sidebar/components/Header/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="fixed left-0 right-0 top-0 w-full">
+    <div
+      class="flex flex-row-reverse justify-around items-center bg-white shadow border-t py-2"
+    >
+      Age Coin
+    </div>
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavbarComponent"
+});
+</script>
---
+++ src/modules/sidebar/components/Navigation/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="px-3 mt-4">
+    <div>
+      <router-link
+        v-for="item in navigation"
+        :key="item.name"
+        :to="item.to"
+        v-slot="{ isActive }"
+        exact
+      >
+        <div
+          :class="[isActive ? 'active' : 'inactive', 'nav-item']"
+          :aria-current="isActive ? 'page' : undefined"
+        >
+          <component
+            :is="item.icon"
+            :class="[isActive ? 'active-icon' : 'inactive-icon', 'icon']"
+            aria-hidden="true"
+          />
+          {{ item.name }}
+        </div>
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { CollectionIcon, TemplateIcon } from "@heroicons/vue/outline";
+
+export default defineComponent({
+  name: "NavigationComponent",
+  components: {
+    CollectionIcon,
+    TemplateIcon
+  },
+  setup() {
+    const navigation = [
+      { name: "Dashboard", to: "/dashboard", icon: TemplateIcon },
+      {
+        name: "Tasks",
+        to: "/tasks",
+        icon: CollectionIcon
+      }
+    ];
+    return { navigation };
+  }
+});
+</script>
+<style src="./style.css" scoped></style>
