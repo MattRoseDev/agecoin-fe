9b22fafea927e1792c2af704c97d549f34293e7c
Matt Rose <mattroseinbox@gmail.com>
2021-05-24 20:40:07 +0430

    feat: add Login view

---
+++ src/views/Login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Login />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Login from "@/modules/login/index.vue";
+
+export default defineComponent({
+  name: "LoginPage",
+  components: {
+    Login
+  }
+});
+</script>
