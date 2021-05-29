845d52fbb15053e2b66b797762be0469cb3628c2
Mahdi <mahdineer@pm.me>
2021-05-30 00:29:12 +0430

    feat: add Profile view

---
+++ src/views/Profile/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full lg:w-2/5 h-screen">
+    <Profile />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Profile from "@/modules/profile/index.vue";
+
+export default defineComponent({
+  name: "ProfilePage",
+  components: {
+    Profile
+  }
+});
+</script>
