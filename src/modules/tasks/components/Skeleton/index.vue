cea471183ebaa3388522f36c1a8edbc7e0ee3df5
Matt Rose <mattroseinbox@gmail.com>
2021-06-11 03:08:54 +0430

    refactor: improve Skeleton component

--- src/modules/tasks/components/Skeleton/index.vue
+++ src/modules/tasks/components/Skeleton/index.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="w-full flex flex-col items-center animate-pulse">
-    <div
-      v-for="item in items"
-      :key="item"
-      class="w-full rounded-lg bg-gray-100 mt-5 p-4 flex flex-col items-start"
-    >
-      <span class="h-10 w-64 bg-gray-200 inline-block rounded-md"></span>
-      <span class="h-3 w-40 bg-gray-200 inline-block rounded-md mt-4"></span>
-      <span class="h-4 w-14 bg-gray-200 inline-block rounded-md mt-4"></span>
-    </div>
+    <TaskSkeleton v-for="item in items" :key="item" class="mt-5" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import TaskSkeleton from "@/components/Skeleton/Task.vue";
 
 export default defineComponent({
   name: "TasksSkeletonComponent",
+  components: {
+    TaskSkeleton
+  },
   setup() {
     const items = new Array(4);
     return { items };
