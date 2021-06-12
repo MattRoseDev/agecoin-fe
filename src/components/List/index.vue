03f0abc6119ed44c13b5966ced34e4ab0d37e1e8
Mahdi <mahdineer@pm.me>
2021-06-12 17:08:52 +0430

    feat: implement List component

---
+++ src/components/List/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <small class="block mb-2 ml-1 font-bold text-gray-600">{{ title }}</small>
+    <div class="bg-white shadow overflow-hidden rounded-lg">
+      <ul class="divide-y divide-gray-200">
+        <li
+          v-for="item in items"
+          :key="item.label"
+          class="px-3 py-3 flex justify-between"
+        >
+          <span class="text-sm font-medium text-gray-700">{{
+            item.label
+          }}</span>
+          <span class="text-sm font-medium text-gray-500">{{
+            item.value
+          }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { ItemType } from './@types'
+
+export default defineComponent({
+  name: "ListComponent",
+  props: {
+    title: String,
+    items: Array as () => ItemType[]
+  }
+});
+</script>
+
+<style scoped></style>
