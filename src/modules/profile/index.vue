20c46ba24fc90e11d666be334f159a38fcb5c35f
Matt Rose <mattroseinbox@gmail.com>
2021-06-12 17:10:44 +0430

    perf: add profile hook

--- src/modules/profile/index.vue
+++ src/modules/profile/index.vue
@@ -19,59 +19,31 @@
     <div v-if="spentCoins" class="mt-5 mx-5">
       <small class="text-gray-600">Spent coins</small>
       <br />
-      <div class="text-4xl font-black">
+      <div class="text-4xl font-bold">
         {{ coinFormat(spentCoins) }}
       </div>
     </div>
-    <div class="mx-5 mt-3">
-      <small class="block mb-2 ml-1 font-bold text-gray-500"
-        >Personal Information</small
-      >
-      <div class="bg-white shadow overflow-hidden rounded-lg">
-        <ul class="divide-y divide-gray-200">
-          <li class="px-3 py-3 flex justify-between">
-            <span class="text-sm font-medium text-gray-700">Email</span>
-            <span class="text-sm font-medium text-gray-500">{{
-              store.state.account.user?.email
-            }}</span>
-          </li>
-          <li class="px-3 py-3 flex justify-between">
-            <span class="text-sm font-medium text-gray-700">Birthday</span>
-            <span class="text-sm font-medium text-gray-500">{{
-              dateFormat(store.state.account.user?.birthday) || "--"
-            }}</span>
-          </li>
-          <li class="px-3 py-3 flex justify-between">
-            <span class="text-sm font-medium text-gray-700">Max age</span>
-            <span class="text-sm font-medium text-gray-500">{{
-              store.state.account.user?.maxAge || "--"
-            }}</span>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <List class="mx-5 mt-3" v-bind="personal" />
+    <List class="mx-5 mt-3" v-bind="ageCoins" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Avatar from "@/components/Avatar/index.vue";
+import List from "@/components/List/index.vue";
 import { CogIcon } from "@heroicons/vue/outline";
-import { useStore } from "@/store";
-import { getStatus } from "@/utils/coins";
-import { coinFormat, dateFormat } from "@/utils/formats";
+import useProfile from "./hooks/useProfile";
 
 export default defineComponent({
   name: "ProfileModule",
   components: {
     Avatar,
+    List,
     CogIcon
   },
   setup() {
-    const store = useStore();
-    const { spentCoins } = getStatus();
-
-    return { store, coinFormat, dateFormat, spentCoins };
+    return useProfile();
   }
 });
 </script>
