96759bb18a36d398f462c94fb54df92f6d203756
Mahdi <mahdineer@pm.me>
2021-06-13 12:18:16 +0430

    feat: add MINUTES and HOURS to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,107 @@
 export const AGE_COIN = 1000 * 60;
+
+export const MINUTES = [
+  {
+    title: "00",
+    value: 0
+  },
+  {
+    title: "05",
+    value: 5
+  },
+  {
+    title: "10",
+    value: 10
+  },
+  {
+    title: "15",
+    value: 15
+  },
+  {
+    title: "20",
+    value: 20
+  },
+  {
+    title: "25",
+    value: 25
+  },
+  {
+    title: "30",
+    value: 30
+  },
+  {
+    title: "35",
+    value: 35
+  },
+  {
+    title: "40",
+    value: 40
+  },
+  {
+    title: "45",
+    value: 45
+  },
+  {
+    title: "50",
+    value: 50
+  },
+  {
+    title: "55",
+    value: 55
+  }
+];
+
+export const HOURS = [
+  {
+    title: "00",
+    value: 0
+  },
+  {
+    title: "01",
+    value: 1
+  },
+  {
+    title: "02",
+    value: 2
+  },
+  {
+    title: "03",
+    value: 3
+  },
+  {
+    title: "04",
+    value: 4
+  },
+  {
+    title: "05",
+    value: 5
+  },
+  {
+    title: "06",
+    value: 6
+  },
+  {
+    title: "07",
+    value: 7
+  },
+  {
+    title: "08",
+    value: 8
+  },
+  {
+    title: "09",
+    value: 9
+  },
+  {
+    title: "10",
+    value: 10
+  },
+  {
+    title: "11",
+    value: 11
+  },
+  {
+    title: "12",
+    value: 12
+  }
+];
