76978cf879d455f6c1fbf40b198948bbc2d9c135
Mahdi <mahdineer@pm.me>
2021-06-14 01:24:27 +0430

    perf: improve utils and add getRouteParams function

---
+++ src/utils/@types/getRouteParams.ts
@@ -0,0 +1,1 @@
+export type GetRouteParamsByKey = (key: string) => string;
--- src/utils/getRouteParams.ts
+++ src/utils/getRouteParams.ts
@@ -1,5 +1,6 @@
 import router from "@/router";
+import { GetRouteParamsByKey } from "./@types/getRouteParams";
 
-export const getRouteParamsByKey = (key: string) => {
+export const getRouteParamsByKey: GetRouteParamsByKey = (key: string) => {
   return router.currentRoute.value.params[key].toString();
 };
