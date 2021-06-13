21f05da548beac9261162e71a73b2e94d78f817a
Matt Rose <mattroseinbox@gmail.com>
2021-06-14 01:09:09 +0430

    feat: add useGetTask hook

---
+++ src/hooks/useGetTask.ts
@@ -0,0 +1,39 @@
+import { useQuery } from "@vue/apollo-composable";
+import { GET_TASK } from "@/graphql/task";
+import { Ref, ref } from "vue";
+import type { Task } from "@/@types/task";
+import { useStore } from "@/store";
+import { MutationType } from "@/@enums/mutations";
+
+type GetTask = { task: Ref<Task>; loading: Ref<boolean> };
+
+export default (taskId: string): GetTask => {
+  const store = useStore();
+  const task = ref<Task>({
+		id: '',
+		title: '',
+		defaultCoins: 0,
+		description: ''
+	});
+  const taskInfo = store.getters.getTaskById(taskId);
+
+  if (taskInfo) {
+    return { loading: ref(false), task: ref(taskInfo) };
+  } else {
+    const { loading, onResult } = useQuery(GET_TASK, {
+      taskId
+    });
+
+    onResult(result => {
+      if (result.data) {
+        const {
+          data: { getTask }
+        } = result;
+				store.commit(MutationType.AppendNewTask, getTask)
+        task.value = getTask;
+      }
+    });
+
+    return { loading, task };
+  }
+};
