21d1de70da4a578779a71610db4d7040201caebf
Mahdi <mahdineer@pm.me>
2021-06-30 22:19:12 +0430

    perf: improve TaskSkeleton component

--- src/components/Skeleton/Task.vue
+++ src/components/Skeleton/Task.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="w-full rounded-lg bg-gray-200 p-4 flex flex-col items-start">
-    <span class="h-10 bg-gray-300 inline-block rounded-md w-48"></span>
-    <span class="h-4 w-16 bg-gray-300 inline-block rounded-md mt-4"></span>
+  <div class="w-full rounded-lg bg-gray-200 dark:bg-gray-800 p-4 flex flex-col">
+    <span
+      class="h-10 bg-gray-300 dark:bg-gray-700 inline-block rounded-md w-48"
+    ></span>
+    <span
+      class="h-4 w-16 bg-gray-300 dark:bg-gray-700 inline-block rounded-md my-4"
+    ></span>
+    <div v-if="buttons" class="flex justify-end w-100">
+      <div class="flex flex-grow justify-start">
+        <span
+          class="w-12 h-12 bg-gray-300 dark:bg-gray-700 inline-block rounded-md"
+        ></span>
+      </div>
+      <div class="flex">
+        <span
+          class="w-12 h-12 bg-gray-300 dark:bg-gray-700 inline-block rounded-md mr-3"
+        ></span>
+        <span
+          class="w-12 h-12 bg-gray-300 dark:bg-gray-700 inline-block rounded-md"
+        ></span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +28,9 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "TaskSkeleton"
+  name: "TaskSkeleton",
+  props: {
+    buttons: Boolean
+  }
 });
 </script>
--- src/modules/task/components/Skeleton/index.vue
+++ src/modules/task/components/Skeleton/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex flex-col items-center animate-pulse">
-    <TaskSkeleton />
+    <TaskSkeleton :buttons="true" />
   </div>
 </template>
 
