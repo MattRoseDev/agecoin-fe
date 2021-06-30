7939246064a4b2c3a8e8d24b3878b315ae1b9699
Mahdi <mahdineer@pm.me>
2021-07-01 03:08:13 +0430

    feat: add Button component for task module

---
+++ src/modules/task/components/Button/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link v-if="to" :to="to" class="link">
+    <slot></slot>
+  </router-link>
+  <button v-else @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ButtonComponent",
+  props: {
+    onClick: Function,
+    to: String
+  }
+});
+</script>
+
+<style src="./style.css" scoped></style>
