6cb86cd9834af30924f5a3d1c1e9b250b9830442
Matt Rose <mattroseinbox@gmail.com>
2021-07-01 03:27:28 +0430

    fix: size of Task Skeleton elements

--- src/components/Skeleton/Task.vue
+++ src/components/Skeleton/Task.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="w-full rounded-lg bg-gray-200 dark:bg-gray-800 p-4 flex flex-col">
     <span
-      class="h-10 bg-gray-300 dark:bg-gray-700 inline-block rounded-md w-48"
+      class="h-8 bg-gray-300 dark:bg-gray-700 inline-block rounded-md w-48"
     ></span>
     <span
       class="h-4 w-16 bg-gray-300 dark:bg-gray-700 inline-block rounded-md my-4"
     ></span>
-    <div v-if="buttons" class="flex w-100">
+    <div v-if="buttons" class="flex items-end w-100">
       <div class="flex">
         <span
-          class="w-12 h-12 bg-gray-300 dark:bg-gray-700 inline-block rounded-md mr-3"
+          class="w-11 h-11 bg-gray-300 dark:bg-gray-700 inline-block rounded-md mr-3"
         ></span>
         <span
-          class="w-12 h-12 bg-gray-300 dark:bg-gray-700 inline-block rounded-md"
+          class="w-11 h-11 bg-gray-300 dark:bg-gray-700 inline-block rounded-md"
         ></span>
       </div>
       <div class="flex flex-grow justify-end">
         <span
-          class="w-12 h-12 bg-gray-300 dark:bg-gray-700 inline-block rounded-md"
+          class="w-9 h-9 bg-gray-300 dark:bg-gray-700 inline-block rounded-md"
         ></span>
       </div>
     </div>
