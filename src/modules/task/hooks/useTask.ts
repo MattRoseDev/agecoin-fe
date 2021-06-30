104edf314e4b0e61390c7f796508478bfd55a8a3
Mahdi <mahdineer@pm.me>
2021-07-01 03:09:40 +0430

    feat: add useFooter hook for task module

--- src/modules/task/hooks/useTask.ts
+++ src/modules/task/hooks/useTask.ts
@@ -1,22 +1,10 @@
 import { getRouteParamsByKey } from "@/utils/getRouteParams";
 import useGetTask from "@/hooks/useGetTask";
-import { Ref, ref } from "vue";
-import { Task } from "@/@types/task";
-
-type UseTask = {
-  task: Ref<Task>;
-  loading: Ref<boolean>;
-  showDelete: Ref<boolean>;
-  toggleShowDelete: () => void;
-};
+import { UseTask } from "../@types";
 
 export default (): UseTask => {
-  const showDelete = ref(false);
   const taskId = getRouteParamsByKey("taskId");
   const { task, loading } = useGetTask(taskId);
 
-  const toggleShowDelete = () => {
-    showDelete.value = !showDelete.value;
-  };
-  return { loading, task, showDelete, toggleShowDelete };
+  return { loading, task };
 };
