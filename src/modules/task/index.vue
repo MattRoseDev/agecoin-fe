99f4291f028e551774c7466a9e6a633f551f1ba7
Matt Rose <mattroseinbox@gmail.com>
2021-07-04 19:43:51 +0430

    perf: improve styles for task module

--- src/modules/task/index.vue
+++ src/modules/task/index.vue
@@ -1,36 +1,48 @@
 <template>
-  <div class="cursor-pointer" @click="showFooter">
+  <div>
     <div v-if="task">
       <div
-        class="flex flex-col pb-3 lg:pb-4 bg-white dark:bg-gray-800 shadow rounded-lg w-full"
+        :class="[
+          task.status == 2 && 'opacity-50',
+          'flex flex-col pb-3 lg:pb-4 bg-white dark:bg-gray-800 shadow rounded-lg w-full'
+        ]"
       >
-        <div class="flex mx-4 mt-4 items-end ">
-          <div
-            :class="[
-              task.status == 2 && 'line-through',
-              'text-3xl flex-grow mb-0 font-bold dark:text-white'
-            ]"
-            v-if="task?.title"
-          >
-            {{ task.title }}
-          </div>
-        </div>
-        <p
-          class="text-sm mx-4 mt-3 font-medium dark:text-white"
-          v-if="task?.description"
-        >
-          {{ task.description }}
-        </p>
-        <div class="relative mx-4">
-          <div v-if="task?.coins" class="mt-2">
-            <small
-              class="text-xs inline-block text-gray-500 dark:text-gray-300"
+        <div @click="toggleExpandTask" class="cursor-pointer">
+          <div class="flex mx-4 mt-4 items-start">
+            <div
+              :class="[
+                task.status == 2 && 'line-through',
+                'text-3xl flex-grow mb-0 font-bold dark:text-white'
+              ]"
+              v-if="task?.title"
             >
-              <span class="text-sm">{{ task.coins }}</span> coins
-            </small>
+              {{ task.title }}
+            </div>
+            <span class="text-gray-500"
+              ><ChevronDownIcon
+                :class="[
+                  shouldCompleteView && 'rotate-180',
+                  'w-4 h-4 transform duration-300'
+                ]"
+            /></span>
+          </div>
+          <p
+            class="text-sm mx-4 mt-3 font-medium dark:text-white"
+            v-if="task?.description"
+          >
+            {{ task.description }}
+          </p>
+          <div class="relative mx-4">
+            <div v-if="task?.coins" class="mt-2">
+              <small
+                class="text-xs inline-block text-gray-500 dark:text-gray-300"
+              >
+                <span class="text-sm">{{ task.coins }}</span> coins
+              </small>
+            </div>
           </div>
         </div>
-        <Footer v-if="shouldShowFooter || task.active" :task="task" />
+        <Footer v-if="shouldCompleteView" :task="task" />
       </div>
     </div>
   </div>
@@ -40,22 +52,25 @@
 import { defineComponent, ref } from "vue";
 import Footer from "./components/Footer/index.vue";
 import { Task } from "@/@types/task";
+import { ChevronDownIcon } from "@heroicons/vue/outline";
 
 export default defineComponent({
   name: "TaskModule",
   components: {
-    Footer
+    Footer,
+    ChevronDownIcon
   },
   props: {
     task: Object as () => Task,
     completeView: Boolean
   },
   setup(props) {
-    const shouldShowFooter = ref(props.completeView);
+    const shouldCompleteView = ref(props.completeView);
 
-    const showFooter = () => (shouldShowFooter.value = true);
+    const toggleExpandTask = () =>
+      (shouldCompleteView.value = !shouldCompleteView.value);
 
-    return { shouldShowFooter, showFooter };
+    return { shouldCompleteView, toggleExpandTask };
   }
 });
 </script>
