cdc2fc1995c4135282d2460e19d77d61cb03a773
Mahdi <mahdineer@pm.me>
2021-07-16 02:26:33 +0430

    feat: initialize DatePicker component

---
+++ src/components/DatePicker/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <DatePicker v-model="date" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { DatePicker } from "v-calendar";
+
+export default defineComponent({
+  name: "DatePicker",
+  components: {
+    DatePicker
+  },
+  setup() {
+    const date = ref(new Date());
+
+    return { date };
+  }
+});
+</script>
