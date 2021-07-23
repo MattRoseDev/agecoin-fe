74a77b0e9bb87845934a3b37670b8b701fb78c24
Mahdi <mahdineer@pm.me>
2021-07-24 00:21:05 +0430

    perf: improve tasks mutation

--- src/modules/sidebar/components/UserInfo/UserInfoSkeleton.vue
+++ src/modules/sidebar/components/UserInfo/UserInfoSkeleton.vue
@@ -4,9 +4,8 @@
       <Avatar class="w-10 h-10" />
       <span class="flex-1 flex flex-col min-w-0 h-10 space-y-2 ">
         <span class="bg-gray-300 dark:bg-gray-700 w-32 h-4 rounded-md"> </span>
-        <span class="bg-gray-300 dark:bg-gray-700 w-24 h-4 rounded-md">
-        </span> </span
-      >x
+        <span class="bg-gray-300 dark:bg-gray-700 w-24 h-4 rounded-md"> </span>
+      </span>
     </div>
   </span>
 </template>
